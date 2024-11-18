8b909979ea9152343d41a5bea47f87b0106c6e98
Diarduriqi <diar.roitiv@gmail.com>
2024-11-18 13:38:57 +0100

    d1vr1

--- src/page/SingleApartmentPage.jsx
+++ src/page/SingleApartmentPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 import SingleApartment from "../components/SingleApartment/SingleApartment";
 import GallerySlider from "../components/common/GallerySlider";
@@ -13,7 +13,56 @@ const images = [
 const SingleApartmentPage = () => {
   return (
     <Box>
-      <Box sx={{ height: "450px", backgroundColor: "#1d1d3a" }}>Headeri</Box>
+      <Box
+        sx={{
+          height: "450px",
+          backgroundColor: "#1d1d3a",
+          display: "flex",
+          flexDirection: "column",
+          padding: "150px 50px 0 50px",
+          gap: "30px",
+        }}
+      >
+        <Box>
+          <Button
+            sx={{
+              width: "240px",
+              backgroundColor: "white",
+              color: "#1d1d3a",
+              borderRadius: "50px",
+              height: "55px",
+            }}
+          >
+            {" "}
+            <img
+              src="/assets/images/vector1.png"
+              alt=""
+              style={{ marginRight: "10px" }}
+            />
+            Kthehu pas
+          </Button>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            sx={{
+              fontFamily: "poppins",
+              fontWeight: "600",
+              fontSize: "70px",
+              color: "#C1AC40",
+            }}
+          >
+            Apartamenti: <span style={{ color: "white" }}>A10</span>
+          </Typography>
+        </Box>
+      </Box>
       <Box sx={{ padding: "50px" }}>
         <SingleApartment />
       </Box>
