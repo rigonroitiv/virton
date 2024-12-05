c7d30e72a981d71de00fbc06ee0e4938c8429c33
Diarduriqi <diar.roitiv@gmail.com>
2024-12-05 11:53:12 +0100

    update popover i thjesht

--- src/components/popup/ApartmentPopup.jsx
+++ src/components/popup/ApartmentPopup.jsx
@@ -1,19 +1,45 @@
-import { Box, Popover, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Popover, Typography } from "@mui/material";
+import React from "react";
 
-const ApartmentPopup = ( { anchorEl, setPopupMenu, data } ) => {
-    const open = new Boolean(anchorEl);
+const ApartmentPopup = ({ anchorEl, setPopupMenu, data }) => {
+  const open = new Boolean(anchorEl);
   return (
-    <Popover
-    open={open}
-    anchorEl={anchorEl}
-    sx={{pointerEvents: 'none'}}
-    >
-        <Box>
-            <Typography>{data.name}</Typography>
-        </Box>
+    <Popover open={open} anchorEl={anchorEl} sx={{ pointerEvents: "none" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          width: "200px",
+          height: "250px",
+          padding: "20px",
+          border: "3px solid #C1AC40",
+          backgroundColor: "#1d1d3a",
+        }}
+      >
+        <Typography
+          sx={{ fontFamily: "poppins", fontWeight: "400", color: "white" }}
+        >
+          {data.name}{" "}
+        </Typography>
+        <Typography
+          sx={{ fontFamily: "poppins", fontWeight: "400", color: "white" }}
+        >
+          {data.square}
+        </Typography>
+        <Typography
+          sx={{ fontFamily: "poppins", fontWeight: "400", color: "white" }}
+        >
+          {data.floorNumber}
+        </Typography>
+        <Typography
+          sx={{ fontFamily: "poppins", fontWeight: "400", color: "white" }}
+        >
+          {data.rooms}
+        </Typography>
+      </Box>
     </Popover>
-  )
-}
+  );
+};
 
-export default ApartmentPopup
+export default ApartmentPopup;
