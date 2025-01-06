f1878765fb09f472be09fe8f898aceda30385f36
rigonroitiv <rigon.roitiv@gmail.com>
2025-01-06 10:18:43 +0100

    link fixed

--- src/page/GeneralBuilding.jsx
+++ src/page/GeneralBuilding.jsx
@@ -114,61 +114,61 @@ const object1 = [
 
 const object2 = [
   {
-    id: 4,
-    position: 4,
+    id: 1,
+    position: 1,
     href: "/apartments/22",
-    imageUrl: `${imagePath}03-2.jpg`,
+    imageUrl: `${imagePath}01-2.jpg`,
     height: "3000",
     width: "1688",
     transform: "translate(1.07) scale(.64)",
     points: [
       {
         d: "M 886.67,916 860,450.67 1290.67,429.33 1372,528 1320,884 Z",
-        href: "/apartments/3",
+        href: "/2/apartments/3",
       },
       {
         d: "m 1381.33,198.67 -34,264 -27.07,2.57 51.74,62.76 -52,356 314.67,-26.67 160,-557.33 -228,-111.33 z",
-        href: "/apartments/3",
+        href: "/2/apartments/3",
       },
       {
         d: "m 1194,76 -22.67,359.24 119.34,-5.91 29.59,35.91 27.07,-2.57 34,-264 18.72,-1.01 L 1411.33,114 1324,72.67 Z",
-        href: "/apartments/3",
+        href: "/2/apartments/3",
       },
       {
         d: "M 355.33,794.67 278.67,722 168,340 l 372.67,-86.67 94,34 v 9.34 L 823.33,244 l 67.78,21.33 110.89,-32.89 95.78,22.89 -8.64,183.99 -229.14,11.35 6.84,119.28 -174.4,86.05 -16,-9.33 z",
-        href: "/apartments/3",
-      },
-    ],
-  },
-  {
-    id: 3,
-    position: 3,
-    imageUrl: `${imagePath}04-2.jpg`,
-    height: "3840",
-    width: "2160",
-    transform: "scale(.5)",
-    points: [
-      {
-        d: "M 1397.67,475 1317.78,939.56 1180.89,1080 835.56,817.33 V 782.22 L 629.78,636.89 594.22,654.22 426.67,524.89 382.22,305.78 l 143.11,-37.34 102.23,46.23 111.11,-32.45 170.22,68 72.44,-26.66 z",
-        href: "/apartments/d",
-      },
-      {
-        d: "M 1416.44,291.11 1296,188 l -141.78,4.44 -12.77,189.36 244.18,88.82 z",
-        href: "/apartments/c",
-      },
-      {
-        d: "m 1015.11,18.67 -1.78,72 21.84,33.83 -4.48,217.01 110.76,40.29 12.77,-189.36 113.12,-3.54 L 1282.33,37 1243,14.67 Z",
-        href: "/apartments/b",
-      },
-      {
-        d: "M 491.67,52 519.33,27 741,21.33 l 6.67,78.34 265.66,-9 21.84,33.83 -4.48,217.01 -49.36,-17.95 -72.44,26.66 -170.22,-68 -111.11,32.45 -102.23,-46.23 z",
-        href: "/apartments/a",
+        href: "/2/apartments/3",
       },
     ],
   },
   {
     id: 2,
     position: 2,
+    imageUrl: `${imagePath}02-2.jpg`,
+    height: "3840",
+    width: "2160",
+    transform: "scale(.5)",
+    points: [
+      {
+        d: "M 1397.67,475 1317.78,939.56 1180.89,1080 835.56,817.33 V 782.22 L 629.78,636.89 594.22,654.22 426.67,524.89 382.22,305.78 l 143.11,-37.34 102.23,46.23 111.11,-32.45 170.22,68 72.44,-26.66 z",
+        href: "/2/apartments/d",
+      },
+      {
+        d: "M 1416.44,291.11 1296,188 l -141.78,4.44 -12.77,189.36 244.18,88.82 z",
+        href: "/2/apartments/c",
+      },
+      {
+        d: "m 1015.11,18.67 -1.78,72 21.84,33.83 -4.48,217.01 110.76,40.29 12.77,-189.36 113.12,-3.54 L 1282.33,37 1243,14.67 Z",
+        href: "/2/apartments/b",
+      },
+      {
+        d: "M 491.67,52 519.33,27 741,21.33 l 6.67,78.34 265.66,-9 21.84,33.83 -4.48,217.01 -49.36,-17.95 -72.44,26.66 -170.22,-68 -111.11,32.45 -102.23,-46.23 z",
+        href: "/2/apartments/a",
+      },
+    ],
+  },
+  {
+    id: 3,
+    position: 3,
     imageUrl: `${imagePath}03-2.jpg`,
     height: "3840",
     width: "2160",
@@ -176,41 +176,41 @@ const object2 = [
     points: [
       {
         d: "M 860.44,972.44 723.56,846.67 689.33,564 995.11,424 v -95.56 l 242.67,-96.44 18.66,6.67 4.89,-59.11 179.11,-64.45 143.56,26.67 -67.56,323.11 -156,113.33 -19.11,-9.78 -168.89,120.89 -3.55,51.11 z",
-        href: "/apartments/a",
+        href: "/2/apartments/a",
       },
       {
         d: "M 328.44,905.33 142.67,310.22 306.22,176.89 456.44,172 l 5.78,-5.78 166.22,-5.78 42.67,328.89 -38.22,181.78 -17.78,1.33 -58.67,216.45 z",
-        href: "/apartments/b",
+        href: "/2/apartments/b",
       },
       {
         d: "m 426.22,77.78 68,-56.89 L 636,17.78 653.33,168 l 75.23,-0.67 -3,13.23 23.44,253.55 -84.69,2.81 -35.87,-276.48 -166.22,5.78 -5.78,5.78 -12.07,0.39 z",
-        href: "/apartments/d",
+        href: "/2/apartments/d",
       },
       {
         d: "m 1011,133 -276.33,8.67 -6.11,25.66 -3,13.23 L 749,434.11 995.11,422 v -93.56 l 31.64,-12.57 V 170 Z",
-        href: "/apartments/c",
+        href: "/2/apartments/c",
       },
     ],
   },
   {
-    id: 1,
-    position: 1,
-    imageUrl: `${imagePath}02-2.jpg`,
+    id: 4,
+    position: 4,
+    imageUrl: `${imagePath}04-2.jpg`,
     height: "3840",
     width: "2160",
     transform: "scale(.5)",
     points: [
       {
         d: "m 288,373.33 85.33,-93.33 452,-12 v 112 l 461.34,-17.33 76,94.66 -70.67,452 L 438.67,988 Z",
-        href: "/apartments/b",
+        href: "/2/apartments/b",
       },
       {
         d: "m 1305.33,252 362.67,-10.67 154.67,80 L 1640,894.67 1305.33,924 1292,909.33 l 70.67,-452 -70.67,-88.02 z",
-        href: "/apartments/a",
+        href: "/2/apartments/a",
       },
       {
         d: "m 753.33,120.89 95.56,-17.78 185.78,40.45 v 56 l 218.66,56 -3.11,51.55 v 1.67 l 18.78,-7.89 29.77,8.81 -6.77,59.61 -5.33,-6.64 L 825.33,380 V 268 l -61.11,1.62 z",
-        href: "/apartments/c",
+        href: "/2/apartments/c",
       },
     ],
   },
@@ -224,7 +224,7 @@ const GeneralBuilding = () => {
 
   const handlePrev = () => {
     if (currentInx === 1) {
-      setCurrentInx(4);
+      setCurrentInx(id === 1 ? 4 : 3);
       return;
     }
     setCurrentInx(currentInx - 1);
@@ -263,9 +263,8 @@ const GeneralBuilding = () => {
           overflow: "auto",
         }}
       /> */}
-
-      {id === 1
-        ? object1.map((item) => {
+      {id === '1'
+        && object1.map((item) => {
             if (currentInx === item.id) {
               return (
                 <svg
@@ -294,14 +293,13 @@ const GeneralBuilding = () => {
               );
             }
           })
-        : null}
-
-      {id === 2
-        ? object2.map((item) => {
+        }
+      {id === '2'
+        && object2.map((item) => {
             if (currentInx === item.id) {
               return (
                 <svg
-                  viewBox="0 0 1440 710"
+                  viewBox="0 0 1920 1080"
                   xmlnsXlink="http://www.w3.org/1999/xlink"
                   xmlns="http://www.w3.org/2000/svg"
                   xmlnsSvg="http://www.w3.org/2000/svg"
@@ -310,8 +308,6 @@ const GeneralBuilding = () => {
                   }}
                 >
                   <image
-                    height={item.height}
-                    width={item.width}
                     transform={item.transform}
                     xlinkHref={item.imageUrl}
                   />
@@ -325,8 +321,7 @@ const GeneralBuilding = () => {
                 </svg>
               );
             }
-          })
-        : null}
+          })}
       <IconButton
         onClick={handlePrev}
         sx={{
