8fd4b2666bb9a176336bbeda97ea545af252eee9
Diarduriqi <diar.roitiv@gmail.com>
2025-02-06 16:00:00 +0100

    updateee <3

---
+++ src/components/admin/sales/AdmPaymentModal.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { Box, Button, Grid, Modal, TextField, Typography } from "@mui/material";
+import { Divider } from "@mui/material";
+import axiosInstance from "../../auth/axiosInstance";
+import { BASE_URL, EURO_SYMBOL } from "../../../utils/consts";
+import { toast } from "react-toastify";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -45%)",
+  width: 900,
+  bgcolor: "background.paper",
+  borderRadius: 2,
+  boxShadow: 40,
+  p: 4,
+};
+
+const AdmPaymentModal = ({ open, onClose, sale }) => {
+  const [value, setValue] = useState(null);
+  const addNewKesti = () => {
+    axiosInstance
+      .put(`${BASE_URL}/api/v1/sales?id=${sale.id}&paid=${value}`)
+      .then((res) => {
+        toast.success("U rregullua me sukses");
+      })
+      .catch((error) => {
+        toast.error("Diqka shkoi gabim!");
+      });
+  };
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      sx={{
+        ":focus-visible": {
+          outline: "none",
+        },
+      }}
+    >
+      <Box sx={style}>
+        <Box display={"flex"} gap={2} flexDirection={"column"}>
+          <Typography sx={{ fontSize: "25px" }}>
+            Totali: {sale?.total}
+            {EURO_SYMBOL}{" "}
+          </Typography>
+
+          <Box sx={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+            {" "}
+            <Typography sx={{ fontSize: "25px" }}>
+              Shuma e paguar: {sale?.paid}
+              {EURO_SYMBOL}
+            </Typography>
+            <Typography sx={{ fontSize: "25px" }}>
+              Shuma e mbetur: {sale?.remainingAmount}
+              {EURO_SYMBOL}
+            </Typography>
+          </Box>
+          <TextField
+            value={value}
+            size="medium"
+            label="Kesti radhes"
+            onChange={(e) => setValue(e.currentTarget.value)}
+          />
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "10px",
+              width: "50%",
+            }}
+          >
+            <Button
+              variant="contained"
+              onClick={addNewKesti}
+              sx={{
+                width: "50%",
+                backgroundColor: "#1d1d3a",
+                ":hover": {
+                  backgroundColor: "#c1ac40",
+                },
+              }}
+            >
+              Ruaj
+            </Button>
+            <Button
+              onClick={onClose}
+              variant="contained"
+              sx={{
+                width: "50%",
+
+                backgroundColor: "#1d1d3a",
+                ":hover": {
+                  backgroundColor: "#c1ac40",
+                },
+              }}
+            >
+              Anulo
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
+
+export default AdmPaymentModal;
--- src/components/admin/sales/AdmSalesTable.jsx
+++ src/components/admin/sales/AdmSalesTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -8,13 +8,55 @@ import {
   TableRow,
   Paper,
   IconButton,
+  Collapse,
 } from "@mui/material";
-import { DeleteForever, Edit, Paid } from "@mui/icons-material";
+import {
+  DeleteForever,
+  Edit,
+  Paid,
+  ExpandMore,
+  ExpandLess,
+} from "@mui/icons-material";
 import { useDispatch } from "react-redux";
 import { setApartmentEditModalState } from "../../../features/apartment/ApartmentEditSlice";
+import axios from "axios";
+import { BASE_URL, EURO_SYMBOL } from "../../../utils/consts";
+import moment from "moment";
+import AdmPaymentModal from "./AdmPaymentModal";
+
+const apiPath = "/api/v1/sales";
 
 const AdmSalesTable = () => {
   const dispatch = useDispatch();
+  const [salesData, setSalesData] = useState([]);
+  const [selectedSale, setSelectedSale] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [expandedRows, setExpandedRows] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = (saleItem) => {
+    setSelectedSale(saleItem);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedSale(null);
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}${apiPath}`).then((res) => {
+      setSalesData(res.data);
+    });
+  }, []);
+
+  const toggleRow = (index) => {
+    setExpandedRows((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
 
   return (
     <TableContainer style={{ overflow: "auto", marginTop: "15px" }}>
@@ -36,15 +78,12 @@ const AdmSalesTable = () => {
             <TableCell sx={{ bgcolor: "#1d1d3a", color: "white" }}>
               Totali
             </TableCell>
-
             <TableCell sx={{ bgcolor: "#1d1d3a", color: "white" }}>
               Data
             </TableCell>
-
             <TableCell sx={{ bgcolor: "#1d1d3a", color: "white" }}>
               Pagoi
             </TableCell>
-
             <TableCell sx={{ bgcolor: "#1d1d3a", color: "white" }}>
               Mbetje
             </TableCell>
@@ -54,52 +93,105 @@ const AdmSalesTable = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {Array.from({ length: 50 }, (_, index) => (
-            <TableRow key={index}>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                {index + 1}
-              </TableCell>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                Dummy Apartment {index + 1}
-              </TableCell>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                Dummy Area {index + 1}
-              </TableCell>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                Dummy Sale Status {index + 1}
-              </TableCell>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                Dummy Sale Status {index + 1}
-              </TableCell>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                Dummy Sale Status {index + 1}
-              </TableCell>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                Dummy Sale Status {index + 1}
-              </TableCell>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                Dummy Sale Status {index + 1}
-              </TableCell>
-              <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
-                <IconButton color="primary" size="small">
-                  <Paid />
-                </IconButton>
-                <IconButton
-                  onClick={() => {
-                    dispatch(setApartmentEditModalState(true));
-                  }}
-                  color="warning"
-                  size="small"
-                >
-                  <Edit />
-                </IconButton>
-                <IconButton color="error" size="small">
-                  <DeleteForever />
-                </IconButton>
-              </TableCell>
-            </TableRow>
-          ))}
+          {salesData.map((item, index) => {
+            const isExpanded = expandedRows[index] || false;
+
+            return (
+              <React.Fragment key={index}>
+                <TableRow>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
+                    {index + 1}
+                  </TableCell>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
+                    {item.clientName}
+                  </TableCell>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>{}</TableCell>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
+                    {item.apartmentName}
+                  </TableCell>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
+                    {item.total}
+                  </TableCell>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
+                    {moment(item.createdAt).format("DD-MM-yyyy")}
+                  </TableCell>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
+                    {item.paid}
+                    {EURO_SYMBOL}
+                  </TableCell>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
+                    {item.remainingAmount}
+                    {EURO_SYMBOL}
+                  </TableCell>
+                  <TableCell sx={{ p: 0, pl: "5px", py: "5px" }}>
+                    <IconButton
+                      onClick={() => handleOpenModal(item)}
+                      color="primary"
+                      size="small"
+                    >
+                      <Paid />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => {
+                        dispatch(setApartmentEditModalState(true));
+                      }}
+                      color="warning"
+                      size="small"
+                    >
+                      <Edit />
+                    </IconButton>
+                    <IconButton color="error" size="small">
+                      <DeleteForever />
+                    </IconButton>
+                    <IconButton onClick={() => toggleRow(index)} size="small">
+                      {isExpanded ? <ExpandLess /> : <ExpandMore />}
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell
+                    style={{ paddingBottom: 0, paddingTop: 0 }}
+                    colSpan={9}
+                  >
+                    <Collapse in={isExpanded} timeout="auto" unmountOnExit>
+                      {item?.payments
+                        .slice()
+                        .sort((a, b) => new Date(a.paidAt) - new Date(b.paidAt))
+                        .map((payment, idx) => (
+                          <TableRow key={idx}>
+                            <TableCell colSpan={4}>
+                              <p style={{ textWrap: "nowrap" }}>
+                                Kesti : {idx + 1}
+                              </p>
+                            </TableCell>
+                            <TableCell colSpan={4}>
+                              <p style={{ textWrap: "nowrap" }}>
+                                Shuma e paguar: {payment.amount}
+                                {EURO_SYMBOL}
+                              </p>
+                            </TableCell>
+                            <TableCell colSpan={4}>
+                              <p style={{ textWrap: "nowrap" }}>
+                                Data e pageses:{" "}
+                                {moment(payment.paidAt).format(
+                                  "DD-MM-yyyy HH:mm:ss"
+                                )}
+                              </p>
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                    </Collapse>
+                  </TableCell>
+                </TableRow>
+              </React.Fragment>
+            );
+          })}
         </TableBody>
+        <AdmPaymentModal
+          open={isModalOpen}
+          onClose={handleCloseModal}
+          sale={selectedSale}
+        />
       </Table>
     </TableContainer>
   );
